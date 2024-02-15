453b450858983d2348732bda9c36b1bb73a9a986
suryabhan singh <suryabhan.singh.0407@gmail.com>
2024-02-15 12:01:15 +0530

    started working on admin panel backend

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const morgan=require('morgan');
+const helmet=require('helmet');
+const cors=require('cors');
+
+const {}=require('./config/serverConfig');
+
+const app=express();
+
+const startServer=()={
+
+}
